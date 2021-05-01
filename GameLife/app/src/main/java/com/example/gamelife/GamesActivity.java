b03ef9d455ab6e9201747efa31778670da6e56e5
msd11 <msd11@autistici.org>
2021-05-01 17:02:04 +0200

    Setup authentification

---
+++ GameLife/app/src/main/java/com/example/gamelife/GamesActivity.java
@@ -0,0 +1,52 @@
+package com.example.gamelife;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class GamesActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_games);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+            }
+        });
+
+
+        FirebaseAuth.AuthStateListener ff = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    
+                }
+            }
+        };
+    }
+
+
+
+}
