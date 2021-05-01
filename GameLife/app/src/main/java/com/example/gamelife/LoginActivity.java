872254e4b6f1d69010cb0d2c4cc6f2737e2971a0
msd11 <msd11@autistici.org>
2021-05-01 17:47:50 +0200

    Add GamesFragment

---
+++ GameLife/app/src/main/java/com/example/gamelife/LoginActivity.java
@@ -0,0 +1,93 @@
+package com.example.gamelife;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private TextView txtView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        GamesFragment fragment = GamesFragment.newInstance();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction =
+                fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        EditText mailTxt = (EditText) findViewById(R.id.editTextTextPersonName);
+        EditText passwordTxt = (EditText) findViewById(R.id.editTextTextPassword);
+
+        txtView = (TextView) findViewById(R.id.textView);
+        Button clickButton = (Button) findViewById(R.id.button);
+        clickButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccount(mailTxt.getText().toString(),passwordTxt.getText().toString());
+            }
+        });
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null) {
+
+
+        }
+
+    }
+
+    public void createAccount(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Signout", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            txtView.setText(user.getEmail());
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Signout", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            txtView.setText("ERROR");
+                        }
+
+                        // ...
+                    }
+                });
+    }
+}
--- GameLife/app/src/main/java/com/example/gamelife/MainActivity.java
+++ GameLife/app/src/main/java/com/example/gamelife/MainActivity.java
@@ -1,82 +1,36 @@
 package com.example.gamelife;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-    private TextView txtView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAuth = FirebaseAuth.getInstance();
-
-        EditText mailTxt = (EditText) findViewById(R.id.editTextTextPersonName);
-        EditText passwordTxt = (EditText) findViewById(R.id.editTextTextPassword);
-
-        txtView = (TextView) findViewById(R.id.textView);
-        Button clickButton = (Button) findViewById(R.id.button);
-        clickButton.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-             createAccount(mailTxt.getText().toString(),passwordTxt.getText().toString());
-            }
-        });
-    }
+        ActionBar bar = getSupportActionBar();
+        bar.setTitle("Games");
+        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#312051")));
 
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null) {
-            setContentView(R.layout.activity_games);
-
-        }
-
-    }
-
-    public void createAccount(String email, String password){
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("Signout", "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            txtView.setText(user.getEmail());
-
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("Signout", "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            txtView.setText("ERROR");
-                        }
-
-                        // ...
-                    }
-                });
+        GamesFragment fragment = GamesFragment.newInstance();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction =
+                fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 
 }
